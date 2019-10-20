d0a8c6a7acd9adc9e857cc53513a0fe962b27c80
davidmltz <davidmltz@gmail.com>
2019-10-20 13:20:28 -0700

    Update working_with_python-pptx for testing

--- testing_debug/working_with_python-pptx/python-pptx_working_1.py
+++ testing_debug/working_with_python-pptx/python-pptx_working_1.py
@@ -1,1 +1,50 @@
-from pptx import Presentaion
+from pptx import Presentation
+
+#
+# Code explanation follows code here
+#
+
+prs = Presentation
+title_slide_layout = prs.slide_layouts[0]
+slide = prs.slides.add_slide(title_slide_layout)
+
+
+diagram = """
+BUILD A NEW POWERPOINT PRESENTATION WITH A TILE SLIDE AND TEXT
+
+CODE:
+from pptx import Presentation
+
+prs = Presentation()
+title_slide_layout = prs.slide_layouts[0]
+slide = prs.slides.add_slide(title_slide_layout)
+title = slide.shapes.title
+subtitle = slide.placeholders[1]
+
+title.text = "Hello, World!"
+subtitle.text = "python-pptx was here!"
+
+prs.save('test.pptx')
+
+CODE DIAGRAMMED
+ ________________________________
+|                                |<--- prs = Presentation()
+|                                |
+|       ---------------          |<--- title_slide_layout = prs.slide_layouts[0]
+|       | Hello World |          |     title = slide.shapes.title
+|       --------------- <--------|---- title.text = 'Hello World'
+|                                |
+|                                |
+|                                |
+|   -------------------------    |
+|   | python-pptx was here! | <--|---- subtitle = slide.placeholders[1]
+|   -------------------------    |     subtitle.text = 'python-pptx was here!'
+|                                |
+|                                |
+|                                |
+|                                |
+|________________________________|
+
+"""
+
+print(diagram)
